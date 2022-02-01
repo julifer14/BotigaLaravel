92356777e8dc8445dc1c192c8ada5046e8b7fab2
Julian Fernandez <juli.fer14@gmail.com>
2022-02-01 23:03:59 +0100

    retocs estil form afegir producte

--- resources/views/productes/afegir.blade.php
+++ resources/views/productes/afegir.blade.php
@@ -11,37 +11,42 @@ Afegir producte
 
 
 @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
 @endif
 
 @section('contingut')
 
 <form action="{{route("afegir_producte_save")}}" method="post">
     @csrf
-    <label for="nom">Nom del producte:</label>
-    <input type="text" id="nom" name="nom">
-    <br>
-    <label for="descripcio">Descripció:</label>
-    <input type="text" id="descripcio" name="descripcio">
-    <br>
-    <label for="preu">Preu:</label>
-    <input type="number" step="0.01" id="preu" name="preu"> €
-    <br>
-    <label for="categories">Categoria: </label>
-    <select name="categories" id="categories">
-        @foreach ($categories as $c)
+    <div class=" mb-3">
+        <label class="form-label" for="nom">Nom del producte:</label>
+        <input class="form-text form-control form-control-lg" type="text" id="nom" name="nom">
+    </div>
+    <div class=" mb-3">
+        <label class="form-label" for="descripcio">Descripció:</label>
+        <input class="form-text form-control form-control-lg" type="text" id="descripcio" name="descripcio">
+    </div>
+    <div class=" mb-3">
+        <label class="form-label" for="preu">Preu:</label>
+        <input class="form-control form-control-lg" type="number" step="0.01" id="preu" name="preu">
+    </div>
+    <div class="">
+        <label class="form-label" for="categories">Categoria: </label>
+        <select name="categoria" id="categoria" class="form-select form-control form-control-lg">
+            <option selected>Selecciona una categoria</option>
+            @foreach ($categories as $c)
             <option value="{{ $c->id }}">{{$c->nom}} </option>
-        @endforeach
-    </select>
-
+            @endforeach
+        </select>
+    </div>
     <button class="btn btn-primary" type="submit">Enviar</button>
-    
+
 </form>
 
 @stop
